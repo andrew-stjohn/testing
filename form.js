ed53917db0215d474525167c3f7302f51b267bb4
andrew-stjohn <71135937+andrew-stjohn@users.noreply.github.com>
2023-12-12 10:00:57 -0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+jQuery.ajax({
+    url: "https://jsw.ibm.com/plugins/servlet/issueCollectorBootstrap.js?collectorId=1ceb8366&locale=en_US",
+    type: "get",
+    cache: true,
+    dataType: "script"
+});
